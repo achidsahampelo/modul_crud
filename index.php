ceb78155f3e1bde55a2f94826e5c49480344b65f
achidsahampelo <lastrisahampelo@gmail.com>
2022-01-04 05:32:43 -0800

    Tampil Data2

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
  <!DOCTYPE html>
  <html>
  <head>
-    <title>Tugas UAS</title>
+    <title>UAS Web Programming</title>
  </head>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 
